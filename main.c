125f5c31cedcf3c8d734f5ed90149b06e66365df
Sofía <ravennasofia03@gmail.com>
2023-03-31 10:16:51 -0300

    Ejercicio 5, guia 0.

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
+/*A través de una función que reciba por referencia un número entero,
+calcular el cubo de dicho número y mostrarlo en el bloque principal de
+ejecución del programa.*/
+
 #include <stdio.h>
+int cubo(int);
 
 int main() {
-    printf("Hello, World!\n");
+    int num, resultado;
+
+    printf("Ingresar un número: ");
+    scanf("%d", &num);
+    resultado= cubo(num);
+    printf("%d",resultado);
     return 0;
+}
+
+int cubo(int num){
+
+    return (num*num*num);
 }
